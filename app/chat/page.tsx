e3c67124842e53d1b5a887e8ae7b3be1821fc97b
Abdurrahman <dreydare16@gmail.com>
2025-07-09 13:34:18 +0100

    updated to use heroku link again

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -135,16 +135,19 @@ export default function ChatPage() {
     setIsLoading(true);
 
     try {
-      const res = await fetch("https://comsis.onrender.com/api/chat", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          message: currentInput,
-          user_id: currentUserID,
-        }),
-      });
+      const res = await fetch(
+        "https://comsupport-672d7929a24b.herokuapp.com//api/chat",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            message: currentInput,
+            user_id: currentUserID,
+          }),
+        }
+      );
 
       const text = await res.text();
       let response;
